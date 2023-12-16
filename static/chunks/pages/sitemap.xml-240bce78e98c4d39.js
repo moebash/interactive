83111813b469cfb7bb3c979266e560a5065549ba
moebash <moebashkhelo@gmail.com>
2023-12-16 13:27:45 +0400

    first commit

---
+++ static/chunks/pages/sitemap.xml-240bce78e98c4d39.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[164],{4349:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/sitemap.xml",function(){return _(4946)}])},4946:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSP:function(){return t}});var t=!0;u.default=()=>null}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4349)}),_N_E=n.O()}]);
